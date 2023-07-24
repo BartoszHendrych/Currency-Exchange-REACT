a49fd9ee2f9fb048a83f95babf83b5fea1b45ec6
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-07-24 22:56:57 +0200

    created exchange Table

---
+++ src/Form/exchangeTable.js
@@ -0,0 +1,29 @@
+
+const exchangeTable = [
+    {   
+        key: 1,
+        short: "EUR",
+        name: "Euro",
+        rate: 4.50,
+    },
+    { 
+        key: 2,
+        short: "CHF",
+        name: "Frank Szwajcarski",
+        rate: 4.63,
+    },
+    {   
+        key: 3,
+        short: "GBP",
+        name: "Funt Brytyjski",
+        rate: 5.24,
+    },
+    {   
+        key: 4,
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.21,
+    },
+];
+
+export default exchangeTable;

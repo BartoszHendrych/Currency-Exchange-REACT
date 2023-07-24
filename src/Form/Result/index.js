d7d1c2afdf8126910b8d856fdafc58f8e9440245
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-07-24 22:58:58 +0200

    created new file result

---
+++ src/Form/Result/index.js
@@ -0,0 +1,19 @@
+
+const Result = ({ result }) => {
+
+    return (
+        <p>
+        {result && (
+            <>
+                {result.sourceAmount.toFixed(2)} PLN =&nbsp;
+                <strong>
+                    {result.targetAmount.toFixed(2)} {result.currency}
+                </strong>
+            </>
+            )}
+
+        </p>
+    )
+}
+
+export default Result;

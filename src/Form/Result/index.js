9f60d74bbcdf0964f5c405a5b2eacba43205a59d
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-07-26 18:50:20 +0200

    deleted empty line

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,17 +1,15 @@
 
 const Result = ({ result }) => {
-
     return (
         <p>
-        {result && (
-            <>
-                {result.sourceAmount.toFixed(2)} PLN =&nbsp;
-                <strong>
-                    {result.targetAmount.toFixed(2)} {result.currency}
-                </strong>
-            </>
+            {result && (
+                <>
+                    {result.sourceAmount.toFixed(2)} PLN =&nbsp;
+                    <strong>
+                        {result.targetAmount.toFixed(2)} {result.currency}
+                    </strong>
+                </>
             )}
-
         </p>
     )
 }

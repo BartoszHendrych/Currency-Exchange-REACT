a6baa6220187e4d93f194d72ba460a084091b025
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-08-02 00:16:49 +0200

    Added background in Global Style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,16 +1,17 @@
 import { createGlobalStyle } from "styled-components";
+import  background from "./background.jpg";
 
 export const GlobalStyle = createGlobalStyle`
     html {
         box-sizing: border-box;
     }
    
-*, ::after, ::before {
+    *, ::after, ::before {
         box-sizing: inherit;
     }       
 
     body {
-        background-image: url("background.jpg");
+        background-image:url("${background}");
         background-position: center;
         background-size: cover;
         min-height: 100vh;
